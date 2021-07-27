18888c0d37d30e343489fc10f123cb0fed678f1c
Joel <joelbrooks.us@gmail.com>
2021-07-26 19:17:38 -0500

    Bonus

--- script.js
+++ script.js
@@ -4,22 +4,25 @@ window.addEventListener("load", function() {
       response.json().then(function(json) {
         const div = document.getElementById("container");
         let inner = "";
+        json.sort((a1, a2) => a1.hoursInSpace < a2.hoursInSpace);
         for (let i = 0; i < json.length; i++) {
           inner += addHtml(json[i]);
         }
         div.innerHTML= inner;
+        document.getElementById("count").innerHTML = json.length;
       });
     });
   });
   
   function addHtml(data) {
+    
     let astronaut = `
       <div class="astronaut">
       <div class="bio">
         <h3>${data.firstName} ${data.lastName}</h3>
         <ul>
            <li>Hours in space: ${data.hoursInSpace}</li>
-           <li>Active: ${data.active}</li>
+           <li id="${data.active ? "green" : "no"}">Active: ${data.active}</li>
            <li>Skills: ${data.skills.join(", ")}</li>
         </ul>
      </div>
